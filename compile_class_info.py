4baa333d39162be3e013c43b9af7c6d0b21e5b3f
Enoch Chang <enoch@Enochs-MacBook-Pro-3.local>
2018-02-02 15:18:14 -0500

    Add compile_class_info.py

---
+++ compile_class_info.py
@@ -0,0 +1,70 @@
+import csv
+from io import StringIO
+
+filename_list = []
+team_name = ""
+camel_count = 0
+filename = ""
+
+def main():
+    collect_all_csv_filenames()
+
+    write_file = open("everyone.csv","w")
+    global filename
+
+    for filename in filename_list:
+        if filename != "mlp6.csv":
+            csvfile = open(filename,"r")
+            temp = list(csv.reader(csvfile,delimiter=","))
+            global team_name
+            team_name = temp[0][4]
+            read_csv()
+            write_file = open("everyone.csv","a")
+            write_file.write(",".join(temp[0]))
+            write_file.write("\n")
+            write_file.close
+            write_data()
+
+
+    print(camel_count, "entries are in camel case!")
+
+def collect_all_csv_filenames():
+    import glob
+    global filename_list
+    filename_list = glob.glob('*.csv')
+
+def read_csv():
+    global camel_count
+    no_spaces = check_no_spaces()
+    if no_spaces == False:
+        print("Spaces in ", filename)
+    camel_case = check_camel_case()
+    if camel_case == True:
+        print(filename, " did not use camel case.")
+    else:    
+        camel_count += 1
+
+
+def write_data(type='json'):
+    csvfile = open(filename,"r")
+    temp = list(csv.reader(csvfile,delimiter=","))
+    json_filename = filename.replace('.csv','.json')
+    json_file = open(json_filename, "w")
+    json_file.write(",".join(temp[0]))
+    json_file.close
+
+def check_no_spaces():
+    no_spaces=True
+    if " " in team_name:
+        no_spaces=False
+        if team_name[0] == " ":
+            no_spaces=True    
+    return no_spaces
+
+def check_camel_case():
+    return (team_name.islower() or team_name.isupper())
+    
+
+if __name__ == "__main__":
+    main()
+
